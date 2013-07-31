80342d5dd7ef053872bbc733c9178b3c356c0ac9
Willian Carvalho <o.chambs@gmail.com>
2013-07-31 09:42:49 -0300

    rgb to hex function now ignores case

--- rgb-to-hex.js
+++ rgb-to-hex.js
@@ -1,4 +1,4 @@
-var re = /rgb[ ]*\([ ]*(\d{1,3})[ ]*,[ ]*(\d{1,3})[ ]*,[ ]*(\d{1,3})[ ]*\)[ ]*/;
+var re = /rgb[ ]*\([ ]*(\d{1,3})[ ]*,[ ]*(\d{1,3})[ ]*,[ ]*(\d{1,3})[ ]*\)[ ]*/i;
 
 function doConvert(str, a, b, c) {
     return (+a).toString(16) + (+b).toString(16) + (+c).toString(16);
@@ -10,4 +10,4 @@ function rgbToHex(c) {
 
 //testing
 var hex = rgbToHex('rgb(222, 111, 190)');
-console.log('#'+hex);
+console.log('#' + hex);
